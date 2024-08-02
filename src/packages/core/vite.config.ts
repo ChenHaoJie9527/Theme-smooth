bd29103bf3eb21b5586d73c9abbfcf1eefb335ba
ChenHaoJie9527 <a18520145493@163.com>
2024-08-02 16:16:23 +0800

    feat: core包构建

--- src/packages/core/vite.config.ts
+++ src/packages/core/vite.config.ts
@@ -1,8 +1,27 @@
-import { defineConfig } from "vitest/config"
+import { defineConfig } from 'vitest/config'
+import dts from 'vite-plugin-dts'
+import { resolve } from 'path'
 
 export default defineConfig({
-    test: {
-        environment: 'jsdom',
-        include: ['src/**/*.{test,space}.{js,mjd,cjs,ts,mts,cts,jsx,tsx}']
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'src/index.ts'),
+      name: 'ThemeSmoothCore',
+      fileName: 'theme-smooth-core'
+    },
+    rollupOptions: {
+      external: ["tests", "vitest"],
+      output: {
+        globals: {}
+      }
     }
+  },
+  plugins: [dts({
+    exclude: ['**/*.test.ts', '**/*.spec.ts']
+  })],
+  test: {
+    environment: 'jsdom',
+    globals: true,
+    include: ['tests/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}']
+  }
 })
