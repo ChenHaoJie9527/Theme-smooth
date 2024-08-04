bcbfaea8e0172b4d77e20b0abe77365609740dc5
momotom <a18520145493@163.com>
2024-08-04 23:12:09 +0800

    feat: 实现ThemeProvider

---
+++ packages/react/src/providers/ThemeProvider.tsx
@@ -0,0 +1,40 @@
+import { Theme, ThemeManager, ThemeTransitionEffect } from "@theme-smooth/core";
+import { createContext, useEffect, useState } from "react";
+
+interface ThemeContextType {
+  theme: Theme;
+  toggleTheme: () => void;
+  setTheme: (theme: Theme) => void;
+  setTransitionDuration: (duration: number) => void;
+  setTransitionEffect: (effect: ThemeTransitionEffect) => void;
+}
+
+const ThemeContext = createContext<ThemeContextType | undefined>(undefined);
+
+export const ThemeProvider: React.FC<React.PropsWithChildren<{}>> = ({
+  children,
+}) => {
+  const [themeManager] = useState(() => new ThemeManager());
+  const [theme, setTheme] = useState(themeManager.getTheme());
+
+  useEffect(() => {
+    const handleThemeChange = () => setTheme(themeManager.getTheme());
+    window.addEventListener("theme-change", handleThemeChange);
+
+    return () => window.removeEventListener("theme-change", handleThemeChange);
+  }, [themeManager]);
+
+  const value = {
+    theme,
+    toggleTheme: () => themeManager.toggleTheme(),
+    setTheme: (newTheme: Theme) => themeManager.setTheme(newTheme),
+    setTransitionDuration: (duration: number) =>
+      themeManager.setTransitionDuration(duration),
+    setTransitionEffect: (effect: ThemeTransitionEffect) =>
+      themeManager.setTransitionEffect(effect),
+  };
+
+  return (
+    <ThemeContext.Provider value={value}>{children}</ThemeContext.Provider>
+  );
+};
