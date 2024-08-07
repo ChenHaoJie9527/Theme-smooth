e154ee8d81bc52cdf1c5d6d99a5094bf5c0cacc0
ChenHaoJie9527 <a18520145493@163.com>
2024-08-07 11:35:39 +0800

    feat:  packages/vue

---
+++ packages/vue/src/App.vue
@@ -0,0 +1,53 @@
+<template>
+  <div>
+    <h2>Current Theme</h2>
+    <button @click="toggleChange">Toggle Theme</button>
+    <button
+      @click="
+        () => {
+          themeManager.setTheme('light');
+        }
+      "
+    >
+      Light Theme
+    </button>
+    <button
+      @click="
+        () => {
+          themeManager.setTheme('dark');
+        }
+      "
+    >
+      Dark Theme
+    </button>
+
+    <input type="number" placeholder="Transition Duration (ms)" />
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ThemeManager } from "@theme-smooth/core";
+const themeManager = new ThemeManager({
+  transitionEffect: "circle",
+  transitionDuration: 500,
+});
+
+const toggleChange = async () => {
+  await themeManager.toggleTheme();
+};
+</script>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
---
+++ packages/vue/src/main.ts
@@ -0,0 +1,6 @@
+import { createApp } from 'vue'
+import './style.css'
+import App from './App.vue'
+import '@theme-smooth/core/style.css';  // 导入 core 包的样式
+
+createApp(App).mount('#app')
